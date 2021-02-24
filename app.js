d71a95e2dd8558c02846072979a43f3c8139edc0
gediminas <gediminas.braskus@gmail.com>
2021-02-24 15:29:54 +0200

    improving quality of code

--- app.js
+++ app.js
@@ -4,31 +4,36 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  res.send("Hello!");
+})
 
 app.get("/api/getPriceHistory/:startDate/:endDate", (req, res) => {
   const startDate = req.params.startDate;
   const endDate = req.params.endDate;
 
-  const getPriceHistory = async () => {
+  const getPriceHistory = async (startDate, endDate) => {
     try {
       const response = await fetch(
         `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`
       );
       const data = await response.json();
       const entries = Object.entries(data.bpi);
-      let obj = {};
+      let priceHistory = {};
 
       entries.map(([date, price]) => {
-        obj[date] = price * 1000;
+        priceHistory[date] = price * 1000;
       });
-      res.send(obj);
+
+      return res.status(200).json(priceHistory);
     } catch (error) {
       console.error(error);
+      res.status(500).json({
+        error_message: "An error occured fetching data",
+      });
     }
   };
-  getPriceHistory();
+
+  getPriceHistory(startDate, endDate);
 });
 
 app.listen(port, () => {
